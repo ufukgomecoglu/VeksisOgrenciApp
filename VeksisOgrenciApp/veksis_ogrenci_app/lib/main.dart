f665768d09ee7bb7361fb02a922a60ee53a1d1c4
Ufuk Gömeçoğlu <108555386+ufukgomecoglu@users.noreply.github.com>
2024-05-12 10:17:23 +0300

    Profile page has been created.

--- VeksisOgrenciApp/veksis_ogrenci_app/lib/main.dart
+++ VeksisOgrenciApp/veksis_ogrenci_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:veksis_ogrenci_app/firebase_options.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,8 +49,22 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: HexColor('#8EF9F3'),
+        title: Image.asset(
+          'assets/images/VeksisLogo.jpeg',
+          fit: BoxFit.cover,
+          height: 40,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(_createRoute());
+            },
+            icon: const Icon(Icons.account_circle),
+            color: HexColor('#8377D1'),
+            iconSize: 40,
+          )
+        ],
       ),
       body: Center(
         child: Column(
@@ -69,6 +84,38 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => const ProfilPage(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(0.0, 1.0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
+
+class ProfilPage extends StatelessWidget {
+  const ProfilPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Page 2'),
       ),
     );
   }
